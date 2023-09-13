9703cb93999ddedfe3e7a8dd7046e6e891faa08a
Fiona Weber <git@vidister.de>
2023-09-13 15:12:11 +0000

    Update flakes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "wanda - WAN Data Aggregator";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
