75d60d1facdeb878266ff4c660c6f19cce9a4923
Yureka <yuka@yuka.dev>
2024-06-12 17:49:11 +0200

    update dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "wanda - WAN Data Aggregator";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
