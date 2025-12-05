bf972818f4a46cd02ff3014dbbfdf7ba0848e8b7
Johann Wagner <johann.wagner@wobcom.de>
2025-12-05 10:21:00 +0100

    feat: Use AS-SETs correctly

--- wanda/autonomous_system/autonomous_system.py
+++ wanda/autonomous_system/autonomous_system.py
@@ -23,10 +23,11 @@ class AutonomousSystem:
         if self.irr_as_set:
             set_elements = self.irr_as_set.upper().split(" ")
             for set_element in set_elements:
-                match_result = re.findall(r'(AS[^\s,]*)', set_element)
+                match_result = re.findall(r'^(?:([A-Z]+)::)?((?:AS[0-9]+[:]+)?(?:AS-[A-Z0-9-]+))$', set_element)
 
                 if len(match_result) != 0:
-                    return_elements.append(match_result[0])
+                    matches = match_result[0]
+                    return_elements.append(matches[1])
 
         # Note: If there is no IRR names, we fall back to AS1234, but we do this later in the code.
 
